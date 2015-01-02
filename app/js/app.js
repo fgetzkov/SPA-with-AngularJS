164204b730be1a628984cd3b635aac100b143ff4
Philip Getzkov <fgetzkov@gmail.com>
2015-01-02 19:29:17 +0200

    Add controllers,templates and moduls

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('AdsModule',[]);
+var app = angular.module('AdsModule',['ngRoute']).config(function ($routeProvider) {
+	$routeProvider.when('/register',{
+		templateUrl:'templates/register.html',
+		controler:'app'
+	});
+
+	$routeProvider.when('/ads',{
+		templateUrl:'templates/all-ads.html ';
+	})
+
+});
